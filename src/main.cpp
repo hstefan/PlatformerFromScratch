858ebc78ed31a1c74f88dbb0117a587f502e84c2
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-10-11 04:26:55 -0300

    Adding a basic update loop, managing timing with std::chrono.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,48 @@
 #include <SDL.h>
+#include <chrono>
+#include <thread>
 #include <iostream>
+
+void Update(float dt, bool* quit) {
+  SDL_Event evt;
+  while (SDL_PollEvent(&evt)) {
+    switch (evt.type) {
+    case SDL_KEYDOWN:
+      *quit = evt.key.keysym.scancode == SDL_SCANCODE_ESCAPE;
+      break;
+    case SDL_QUIT:
+      *quit = true;
+      break;
+    }
+  }
+}
+
+void RunGameLoop() {
+  using namespace std::chrono;
+  using clock = high_resolution_clock;
+  using FloatSeconds = duration<float>;
+
+  // target update frequency is 60hz
+  const auto period = 1.f / 60.f;
+  const auto targetPeriod = duration_cast<clock::duration>(FloatSeconds(period));
+
+  auto ti = clock::now();
+  auto dt = clock::duration::zero();
+  auto quit = false;
+  while (!quit) {
+    // update our entities or whatever we may have
+    Update(duration_cast<FloatSeconds>(dt).count(), &quit);
+
+    // calculate timing related operations
+    const auto tf = clock::now();
+    dt = tf - ti;
+    const auto idleDuration = targetPeriod - dt;
+    // check in case our iteration lags behind
+    if (idleDuration.count() > 0)
+      std::this_thread::sleep_for(idleDuration);
+    ti = tf;
+  }
+}
 
 int main(int, char*[]) {
   SDL_Init(SDL_INIT_VIDEO);
@@ -17,7 +60,7 @@ int main(int, char*[]) {
     return 1;
   }
 
-  SDL_Delay(2000);
+  RunGameLoop();
 
   SDL_DestroyWindow(window);
   SDL_Quit();
