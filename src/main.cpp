9c0cb29d9ea23a5a6c2a6623471b5e128024a0c6
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-10-11 05:36:37 -0300

    Adding OpenGL context and frame clearing.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include <GL/glew.h>
+
+void CheckSDLError() {
+#ifndef NDEBUG
+  auto error = SDL_GetError();
+  if (*error != '\0')
+  {
+    std::cout << "SDL error: " << error << "\n";
+    SDL_ClearError();
+  }
+#endif
+}
 
 void Update(float dt, bool* quit) {
   SDL_Event evt;
@@ -17,7 +29,7 @@ void Update(float dt, bool* quit) {
   }
 }
 
-void RunGameLoop() {
+void RunGameLoop(SDL_Window* window) {
   using namespace std::chrono;
   using clock = high_resolution_clock;
   using FloatSeconds = duration<float>;
@@ -30,8 +42,17 @@ void RunGameLoop() {
   auto dt = clock::duration::zero();
   auto quit = false;
   while (!quit) {
+    // clear window
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    CheckSDLError();
+
     // update our entities or whatever we may have
     Update(duration_cast<FloatSeconds>(dt).count(), &quit);
+
+    //swap buffers
+    SDL_GL_SwapWindow(window);
+    CheckSDLError();
 
     // calculate timing related operations
     const auto tf = clock::now();
@@ -44,25 +65,44 @@ void RunGameLoop() {
   }
 }
 
-int main(int, char*[]) {
+bool InitializeContext(SDL_Window** window) {
   SDL_Init(SDL_INIT_VIDEO);
-  
-  auto window = SDL_CreateWindow("A Platformer From Scratch",
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+  *window = SDL_CreateWindow("A Platformer From Scratch",
     SDL_WINDOWPOS_UNDEFINED,
     SDL_WINDOWPOS_UNDEFINED,
     1280,
     720,
     SDL_WINDOW_OPENGL);
-  
+  CheckSDLError();
+
   if (window == nullptr) {
     //TODO: error reporting mechanisms
     std::cout << "Unable to create window " << SDL_GetError() << "\n";
-    return 1;
+    return false;
   }
 
-  RunGameLoop();
+  auto context = SDL_GL_CreateContext(*window);
+  SDL_GL_MakeCurrent(*window, context);
 
-  SDL_DestroyWindow(window);
+  return true;
+}
+
+int main(int, char*[]) {
+  SDL_Window* window = nullptr;
+
+  if (InitializeContext(&window)) {
+    RunGameLoop(window);
+    SDL_GL_DeleteContext(SDL_GL_GetCurrentContext());
+    SDL_DestroyWindow(window);
+  }
+
   SDL_Quit();
 
   return 0;
